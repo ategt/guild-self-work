1e8ecf3220ba0e98ea18f650a9f3ac3f01e6423f
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-19 18:04:17 -0400

    ATSW-5 - Added a class file which outputs a schedule of classes and teachers.

---
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/StillUsingVariables.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.programmingbydoing;
+
+/**
+ *
+ * @author apprentice
+ */
+public class StillUsingVariables {
+
+    public static void main(String[] args) {
+        String myName;
+        short myYearOfGraduation;
+
+        myName = "Adam Tegtmeier";
+        myYearOfGraduation = 2005;
+
+        System.out.println("My name is " + myName + " and I'll graduate in "
+                + myYearOfGraduation + ".");
+    }
+}
