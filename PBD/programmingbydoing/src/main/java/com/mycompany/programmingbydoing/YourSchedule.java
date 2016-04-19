75787687d8cf8855aec11d0af051ef24bbe06806
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-19 18:51:15 -0400

    ATSW-5 - Fixed the spacing so the table lines up.

--- PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/YourSchedule.java
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/YourSchedule.java
@@ -10,12 +10,11 @@ package com.mycompany.programmingbydoing;
  * @author apprentice
  */
 public class YourSchedule {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String course1, course2, course3, course4, course5, course6, course7, course8;
         String teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8;
-        
-        int lineNumber = 1;
-        
+
         course1 = "English III";
         course2 = "Precalculus";
         course3 = "Music Theory";
@@ -24,7 +23,7 @@ public class YourSchedule {
         course6 = "Latin II";
         course7 = "AP US History";
         course8 = "Business Computer Information Systems";
-        
+
         teacher1 = "Ms. Lapan";
         teacher2 = "Mrs. Gideon";
         teacher3 = "Mr. Davis";
@@ -33,24 +32,16 @@ public class YourSchedule {
         teacher6 = "Mrs. Barnett";
         teacher7 = "Ms. Johannessen";
         teacher8 = "Mr. James";
-        
-        System.out.println("+--------------------------------------------------+");
-        System.out.println("| " + lineNumber + " | " + course1 + " | " + teacher1 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course2 + " | " + teacher2 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course3 + " | " + teacher3 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course4 + " | " + teacher4 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course5 + " | " + teacher5 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course6 + " | " + teacher6 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course7 + " | " + teacher7 + " |");
-        lineNumber++;
-        System.out.println("| " + lineNumber + " | " + course8 + " | " + teacher8 + " |");
-        lineNumber++;
-        System.out.println("+--------------------------------------------------+");
+
+        System.out.println("+------------------------------------------------------------+");
+        System.out.println("| 1 |                          " + course1 + " |       " + teacher1 + " |");
+        System.out.println("| 2 |                          " + course2 + " |     " + teacher2 + " |");
+        System.out.println("| 3 |                         " + course3 + " |       " + teacher3 + " |");
+        System.out.println("| 4 |                        " + course4 + " |      " + teacher4 + " |");
+        System.out.println("| 5 |           " + course5 + " |      " + teacher5 + " |");
+        System.out.println("| 6 |                             " + course6 + " |    " + teacher6 + " |");
+        System.out.println("| 7 |                        " + course7 + " | " + teacher7 + " |");
+        System.out.println("| 8 |" + course8 + " |       " + teacher8 + " |");
+        System.out.println("+------------------------------------------------------------+");
     }
 }
