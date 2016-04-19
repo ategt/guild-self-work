236043b90db382e7f1e179e2d406aa6deb5c981b
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-19 18:01:17 -0400

    ATSW-4 - Added a class which outputs a room number, a decimal and a text message.

---
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/UsingVariables.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.programmingbydoing;
+
+/**
+ *
+ * @author apprentice
+ */
+public class UsingVariables {
+    public static void main(String[] args){
+        int myInteger;
+        double myDouble;
+        String myString;
+        
+        myInteger = 113;
+        myDouble = 2.71828;
+        myString = "Computer Science";
+        
+        System.out.println("This is room # " + myInteger);
+        System.out.println("e is close to " + myDouble);
+        System.out.println("I am learning a bit about " + myString );
+    }
+}
