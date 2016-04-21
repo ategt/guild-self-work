405d12103c0d21a1d8669b2a39d6de526e9c57a6
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-21 09:07:37 -0400

    ATSW-7 Did the first bonus in PBD 20.

--- PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/BMICalc.java
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/BMICalc.java
@@ -15,13 +15,23 @@ public class BMICalc {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
         
-        System.out.print("Your height in m: ");
-        float height = keyboard.nextFloat();
+//        System.out.print("Your height in m: ");
+//        float height = keyboard.nextFloat();
+//        
+//        System.out.print("Your weight in kg: ");
+//        float weight = keyboard.nextFloat();
+//        
+//        System.out.println();
         
-        System.out.print("Your weight in kg: ");
-        float weight = keyboard.nextFloat();
+
+        System.out.print("Your height in inches: ");
+        float imperialHeight = keyboard.nextFloat();
         
-        System.out.println();
+        System.out.print("Your weight in pounds: ");
+        float imperialWeight = keyboard.nextFloat();
+        
+        float height = imperialHeight / 39.370f;
+        float weight = imperialWeight / 2.2f;
         
         System.out.println("Your BMI is " + weight / (height * height));
         
