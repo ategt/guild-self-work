d3435d0d514d43cf21d7267160a531e9dd2fb42a
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-21 09:13:36 -0400

    ATSW-7 Did the second bonus in PBD 20.

--- PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/BMICalc.java
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/BMICalc.java
@@ -12,30 +12,44 @@ import java.util.Scanner;
  * @author apprentice
  */
 public class BMICalc {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        
+
+        //  First part of PBD-20
 //        System.out.print("Your height in m: ");
 //        float height = keyboard.nextFloat();
 //        
 //        System.out.print("Your weight in kg: ");
 //        float weight = keyboard.nextFloat();
 //        
-//        System.out.println();
-        
 
-        System.out.print("Your height in inches: ");
-        float imperialHeight = keyboard.nextFloat();
-        
+        // Second part of PBD-20
+//        System.out.println();
+//        System.out.print("Your height in inches: ");
+//        float imperialHeight = keyboard.nextFloat();
+//        
+//        System.out.print("Your weight in pounds: ");
+//        float imperialWeight = keyboard.nextFloat();
+//        
+
+        // Third part of PBD-20
+        System.out.print("Your height (feet only): ");
+        float imperialHeightInFeet = keyboard.nextFloat();
+
+        System.out.print("Your height (inches only): ");
+        float imperialHeightInInches = keyboard.nextFloat();
+
         System.out.print("Your weight in pounds: ");
         float imperialWeight = keyboard.nextFloat();
+
+        float imperialHeight = ( imperialHeightInFeet * 12 ) + imperialHeightInInches;
         
         float height = imperialHeight / 39.370f;
         float weight = imperialWeight / 2.2f;
-        
+
         System.out.println("Your BMI is " + weight / (height * height));
-        
-        
+
     }
-    
+
 }
