310fa6c5c6f972ac36a90b21ec41305cbd6eeb3b
Adam Tegtmeier <ategtmei@gmail.com>
2016-04-21 09:24:30 -0400

    ATSW-8 Modified WhatIf.java according to PBD-21.

--- PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/WhatIf.java
+++ PBD/programmingbydoing/src/main/java/com/mycompany/programmingbydoing/WhatIf.java
@@ -14,7 +14,7 @@ public class WhatIf {
     public static void main(String[] args) {
 
         int people = 20;
-        int cats = 30;
+        int cats = people;
         int dogs = 15;
 
         if (people < cats) {
