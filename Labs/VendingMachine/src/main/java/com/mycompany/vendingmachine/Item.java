7e887f2e6ce601278a59e7ba29a8252b61aef05a
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-04 17:05:23 -0400

    Games starting.

---
+++ Labs/VendingMachine/src/main/java/com/mycompany/vendingmachine/Item.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.vendingmachine;
+
+/**
+ *
+ * @author apprentice
+ */
+public class Item {
+    private String itemName;
+    private int itemCost; // in pennies
+    private int quantityInInventory;
+    private int id;
+
+    /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * @return the itemCost
+     */
+    public int getItemCost() {
+        return itemCost;
+    }
+
+    /**
+     * @return the quantityInInventory
+     */
+    public int getQuantityInInventory() {
+        return quantityInInventory;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @param itemCost the itemCost to set
+     */
+    public void setItemCost(int itemCost) {
+        this.itemCost = itemCost;
+    }
+
+    /**
+     * @param quantityInInventory the quantityInInventory to set
+     */
+    public void setQuantityInInventory(int quantityInInventory) {
+        this.quantityInInventory = quantityInInventory;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
