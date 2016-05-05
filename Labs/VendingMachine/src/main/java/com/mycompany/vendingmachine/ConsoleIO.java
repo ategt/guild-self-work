0f5f5e49e4d07eecef53933b80f173619a7c377e
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-05 12:21:50 -0400

    ATSW-38 ATSW-39 ATSW-40 Currently working on controller for Vending Machine Application.

--- Labs/VendingMachine/src/main/java/com/mycompany/vendingmachine/ConsoleIO.java
+++ Labs/VendingMachine/src/main/java/com/mycompany/vendingmachine/ConsoleIO.java
@@ -41,7 +41,7 @@ public class ConsoleIO {
      * @param choice
      * @return
      */
-    public Number getUserInputValidationLoop(String prompt, int choice) {
+    private Number getUserInputValidationLoop(String prompt, int choice) {
         Number input = 0;
         return getUserInputValidationLoop(input, prompt, choice);
     }
@@ -59,7 +59,7 @@ public class ConsoleIO {
      * @param choice
      * @return
      */
-    public Number getUserInputValidationLoop(Number input, String prompt, int choice) {
+    private Number getUserInputValidationLoop(Number input, String prompt, int choice) {
         boolean isValid = false;
         String inputString;
 
--- Labs/VendingMachine/src/main/java/com/mycompany/vendingmachine/VendingMachineController.java
+++ Labs/VendingMachine/src/main/java/com/mycompany/vendingmachine/VendingMachineController.java
@@ -10,10 +10,78 @@ package com.mycompany.vendingmachine;
  * @author apprentice
  */
 public class VendingMachineController {
-
-ConsoleIO consoleIo = new ConsoleIO();
-Inventory inventory = new Inventory();
-
-
+    
+    ConsoleIO consoleIo = new ConsoleIO();
+    Inventory inventory = new Inventory();
+    int balance;
+    
+    public void run() {
+        
+    }
+    
+    private void vendItem(Item item) {
+        
+        
+        displayChange();
+        
+    }
+    
+    private void inputMoney() {
+        
+        Item selection;
+        int price;
+        
+        balance = consoleIo.getUserIntInputPositive("Please Enter An Amount To Input Into The Simulated Machine:");
+        
+        if (doesMachineHaveInventory()) {
+            
+            displayChart();
+            
+            selection = askForSelection();
+            
+            price = priceSelected(selection);
+            
+            if (price > balance) {
+                informUserOfInsufficentFunds();
+            } else if (doesMachineHaveInventory(selection)) {
+                vendItem(selection);
+            }
+            
+        }
+        
+    }
+    
+    private void selectItem() {
+        
+    }
+    
+    private void refundMoney() {
+        displayChange();
+    }
+    
+    private void displayChange() {
+        
+        Change change = new Change(balance);
+        
+        int quarters = change.getQuarters();
+        int dimes = change.getDimes();
+        int nickles = change.getNickles();
+        int pennies = change.getPennies();
+        
+        String receipt = "Change generated:\n"
+                + " " + quarters + " Quarters\n"
+                + " " + dimes + " Dimes\n"
+                + " " + nickles + " Nickles\n"
+                + " " + pennies + " Pennies\n";
+        
+        consoleIo.printStringToConsole(receipt);
+        balance = 0;
+        
+    }
+    
+    private void informUserOfInsufficentFunds() {
+        
+        consoleIo.printStringToConsole("The Balance Currently In The Machine Is Insufficent For This Action.");
+    }
     
 }
