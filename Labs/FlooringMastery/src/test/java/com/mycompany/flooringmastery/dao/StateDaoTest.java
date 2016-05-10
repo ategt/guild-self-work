26566a3d0205c1391d941017529fe50a4e583822
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-10 10:37:07 -0400

    ATSW-54 ATSW-53 ATSW-52 ATSW-51 ATSW-50 ATSW-49 Cleaned up comments in the stateDao class.

--- Labs/FlooringMastery/src/test/java/com/mycompany/flooringmastery/dao/StateDaoTest.java
+++ Labs/FlooringMastery/src/test/java/com/mycompany/flooringmastery/dao/StateDaoTest.java
@@ -6,7 +6,6 @@
 package com.mycompany.flooringmastery.dao;
 
 import com.mycompany.flooringmastery.dto.State;
-import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -201,7 +200,6 @@ public class StateDaoTest {
         assertTrue(instance.getList().contains("Mexico"));
         assertTrue(instance.getList().contains("GH"));
 
-
     }
 
     @Test
@@ -227,23 +225,6 @@ public class StateDaoTest {
         // Use the update method to save this new text to file.
         stateDao.update(testState);
 
-//        String country = "USA";
-//        String type = "mailing";
-//        String zipCode = "88775";
-//        String poBox = "21";
-//        String firstName = "First and last name here";
-//        String street = "3589 Street Road";
-//        String state = "Michigan";
-        // Set some text in the state file.
-        //returnedState.setNoteString("This Is a test state.");
-//        returnedState.setCity(city);
-//        returnedState.setCountry(country);
-//        returnedState.setType(type);
-//        returnedState.setZipcode(zipCode);
-//        returnedState.setPoBox(poBox);
-//        returnedState.setFirstName(firstName);
-//        returnedState.setStreetState(street);
-//        returnedState.setState(state);
         // Load a new instance of the StateDao.
         StateDao secondDao = new StateDao(true);
 
@@ -265,106 +246,4 @@ public class StateDaoTest {
         assertEquals(thirdDao.get(stateName), null);
 
     }
-
-//    
-//    /**
-//     * Test of create method, of class StateDao.
-//     */
-//    @Test
-//    public void testCreate_State_String() {
-//        System.out.println("create");
-//        State state = null;
-//        String stateName = "";
-//        StateDao instance = new StateDao();
-//        State expResult = null;
-//        State result = instance.create(state, stateName);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of create method, of class StateDao.
-//     */
-//    @Test
-//    public void testCreate_String_State() {
-//        System.out.println("create");
-//        String stateName = "";
-//        State state = null;
-//        StateDao instance = new StateDao();
-//        State expResult = null;
-//        State result = instance.create(stateName, state);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of get method, of class StateDao.
-//     */
-//    @Test
-//    public void testGet() {
-//        System.out.println("get");
-//        String name = "";
-//        StateDao instance = new StateDao();
-//        State expResult = null;
-//        State result = instance.get(name);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of update method, of class StateDao.
-//     */
-//    @Test
-//    public void testUpdate() {
-//        System.out.println("update");
-//        State state = null;
-//        StateDao instance = new StateDao();
-//        instance.update(state);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of delete method, of class StateDao.
-//     */
-//    @Test
-//    public void testDelete() {
-//        System.out.println("delete");
-//        State state = null;
-//        StateDao instance = new StateDao();
-//        instance.delete(state);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getList method, of class StateDao.
-//     */
-//    @Test
-//    public void testGetList() {
-//        System.out.println("getList");
-//        StateDao instance = new StateDao();
-//        List<String> expResult = null;
-//        List<String> result = instance.getList();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of size method, of class StateDao.
-//     */
-//    @Test
-//    public void testSize() {
-//        System.out.println("size");
-//        StateDao instance = new StateDao();
-//        int expResult = 0;
-//        int result = instance.size();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
 }
