bc36180187d56e9866c59a2cdc604671e1ed7d00
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-18 12:17:19 -0400

    I tried to add improved date parsing functionality to ConsoleIO, but the resulting attempt is still to complex to use.

---
+++ Labs/ConsoleIO/src/main/java/com/mycompany/consoleio/exceptions/DateUnSimplifiable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.consoleio.exceptions;
+
+/**
+ *
+ * @author apprentice
+ */
+public class DateUnSimplifiable extends Exception {
+
+    public DateUnSimplifiable(String message) {
+        super(message);
+    }
+
+
+}
---
+++ Labs/ConsoleIO/src/test/java/com/mycompany/consoleio/utilities/DateUtilitiesTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.consoleio.utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author apprentice
+ */
+public class DateUtilitiesTest {
+    
+    public DateUtilitiesTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPrettyDateFromStringUsingBrute method, of class DateUtilities.
+     */
+    @Test
+    public void testGetPrettyDateFromStringUsingBrute() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "01-01-1985";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "01-01-1985";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+    @Test
+    public void testGetPrettyDateFromStringUsingBruteB() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "2 january 2015";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "01-02-2015";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+    @Test
+    public void testGetPrettyDateFromStringUsingBruteC() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "2 dec. 2015";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "12-02-2015";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+    @Test
+    public void testGetPrettyDateFromStringUsingBruteD() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "2 dec 2015";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "12-02-2015";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+    @Test
+    public void testGetPrettyDateFromStringUsingBruteE() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "2 December 2015";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "12-02-2015";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+//    
+//    @Test
+//    public void testGetPrettyDateFromStringUsingBruteG() throws Exception {
+//        System.out.println("getPrettyDateFromStringUsingBrute");
+//        String possibleDate = "2 December 15";
+//        DateUtilities instance = new DateUtilities();
+//        String expResult = "12-02-2015";
+//        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+//        assertEquals(expResult, result);
+//
+//    }
+    
+        @Test
+    public void testGetPrettyDateFromStringUsingBruteF() throws Exception {
+        System.out.println("getPrettyDateFromStringUsingBrute");
+        String possibleDate = "December 25, 2015";
+        DateUtilities instance = new DateUtilities();
+        String expResult = "12-02-2015";
+        String result = instance.getPrettyDateFromStringUsingBrute(possibleDate);
+        assertEquals(expResult, result);
+
+    }
+    
+
+}
