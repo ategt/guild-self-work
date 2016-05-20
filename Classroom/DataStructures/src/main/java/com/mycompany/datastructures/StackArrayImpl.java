2a653e56d24b55cc0595d628aea322e6537e4d0f
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-20 11:53:44 -0400

    About to run first test of ATSW-78.

--- Classroom/DataStructures/src/main/java/com/mycompany/datastructures/StackArrayImpl.java
+++ Classroom/DataStructures/src/main/java/com/mycompany/datastructures/StackArrayImpl.java
@@ -9,28 +9,86 @@ package com.mycompany.datastructures;
  *
  * @author apprentice
  */
-public class StackArrayImpl<T> implements Stack<T>{
+public class StackArrayImpl<T> implements Stack<T> {
 
+    private int pointerNum = 0;
+    private Object[] stack;
+    private float percentToChangeBy = .20f;
+
+    public StackArrayImpl() {
+        stack = new Object[10];
+    }
+
+    public StackArrayImpl(int startingSize) {
+        stack = new Object[startingSize];
+    }
+
+    private void growArray() {
+
+        int newSize = Math.round(stack.length * (1 + percentToChangeBy));
+        System.out.println("Stack was: " + stack.length + "\t Growing to : " + newSize );
+        System.out.println("Pointer is at : " + pointerNum + "\n");
+        Object[] tempStack = java.util.Arrays.copyOf(stack, newSize);
+
+        stack = tempStack;
+
+    }
+
+    private void shrinkArray() {
+        int newSize = Math.round(stack.length * (1 - percentToChangeBy));
+        System.out.println("Stack was: " + stack.length + "\t Shrinking to : " + newSize );
+        System.out.println("Pointer is at : " + pointerNum + "\n");
+        Object[] tempStack = java.util.Arrays.copyOf(stack, newSize);
+        stack = tempStack;
+    }
+
+    private void considerGrowingStack(){
+        
+        if ( stack.length <= pointerNum + 1) {
+            growArray();
+        }
+    }
+    
+    private void considerShrinkingStack(){
+        
+        if ( stack.length * (1 - percentToChangeBy)  >= pointerNum * (1 - (percentToChangeBy*2)) ) {
+            shrinkArray();
+        }
+    }
+    
+    
     @Override
     public void push(T element) {
+        stack[pointerNum] = element;
+        pointerNum++;
 
-        
-
+        considerGrowingStack();
     }
 
     @Override
     public T pop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        considerShrinkingStack();
+        
+        if (pointerNum >= 0) {
+            return (T) stack[pointerNum--];
+        } else {
+            return null;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (pointerNum < 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (pointerNum + 1);
     }
-    
+
 }
