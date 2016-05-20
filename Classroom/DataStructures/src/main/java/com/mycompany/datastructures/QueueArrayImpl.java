2a391f25fbf817644dbca6babe6f81816264b27b
Adam Tegtmeier <ategtmei@gmail.com>
2016-05-20 15:30:50 -0400

    About to start bug testing my ATSW-78 queue.

--- Classroom/DataStructures/src/main/java/com/mycompany/datastructures/QueueArrayImpl.java
+++ Classroom/DataStructures/src/main/java/com/mycompany/datastructures/QueueArrayImpl.java
@@ -11,130 +11,256 @@ package com.mycompany.datastructures;
  */
 public class QueueArrayImpl<T> implements Queue<T> {
 
-    private int pointerNum = 0;
-    private Object[] stack;
+    private int pushNum = 0;
+    private int pullNum = 0;
+    private Object[] queue;
+
     private float percentToChangeBy = .20f;
     private int minimumSize = 0;
 
     public QueueArrayImpl() {
-        stack = new Object[10];
+        queue = new Object[10];
         minimumSize = 10;
     }
 
     public QueueArrayImpl(int startingSize) {
-        stack = new Object[startingSize];
+        queue = new Object[startingSize];
         minimumSize = startingSize;
     }
 
     private void growArray() {
 
-        int newSize = Math.round(stack.length * (1 + percentToChangeBy));
-        Object[] tempStack = java.util.Arrays.copyOf(stack, newSize);
+        boolean wrap = false;
+        int newPush;
+        int newPull;
+        int changeInSize = 0;
 
-        stack = tempStack;
+        int newSize = Math.round(queue.length * (1 + percentToChangeBy));
+
+        if (pushNum > pullNum) {
+            wrap = false;
+
+            // The occupied part does not wrap around.
+            // The new spaces will go on the end.
+            newPull = pullNum;
+            newPush = pushNum;
+
+        } else {
+            wrap = true;
+
+            // The occupied part does wrap around.
+            // The new spaces will go in the middle.
+            newPush = pushNum;
+
+            changeInSize = newSize - queue.length;
+            int pullDistanceFromEnd = queue.length - pullNum;
+
+            newPull = newSize - pullDistanceFromEnd;
+
+        }
+
+        //int  = pushNum - pullNum;
+        //Object[] tempStack = java.util.Arrays.copyOf(queue, newSize);
+        Object[] tempQueue = new Object[newSize];
+
+        if (wrap) {
+            //for ( int i = 0 ; i < queue.length ;  i++ ) {
+
+            for (int i = 0; i < pushNum; i++) {
+                tempQueue[i] = queue[i];
+            }
+
+            // Skip the Blank Spaces
+            //for (int i = changeInSize + pullNum; i < tempQueue.length; i++) {
+            for (int i = pullNum; i < tempQueue.length; i++) {
+                tempQueue[i + changeInSize] = queue[i];
+            }
+        } else {
+            for (int i = 0; i < queue.length; i++) {
+                tempQueue[i] = queue[i];
+            }
+        }
+
+        queue = tempQueue;
+        pushNum = newPush;
+        pullNum = newPull;
 
     }
 
     private void shrinkArray() {
-        int newSize = Math.round(stack.length * (1 - percentToChangeBy));
-        Object[] tempStack = java.util.Arrays.copyOf(stack, newSize);
-        stack = tempStack;
+        int newSize = Math.round(queue.length * (1 - percentToChangeBy));
+
+        boolean wrap = false;
+        int newPush;
+        int newPull;
+        int changeInSize = 0;
+
+        if (pushNum > pullNum) {
+            wrap = false;
+
+            // The occupied part does not wrap around.
+            // The new spaces will go on the end.
+            newPull = pullNum;
+            newPush = pushNum;
+
+        } else {
+            wrap = true;
+
+            // The occupied part does wrap around.
+            // The new spaces will go in the middle.
+            newPush = pushNum;
+
+            changeInSize = newSize - queue.length;
+            int pullDistanceFromEnd = queue.length - pullNum;
+
+            newPull = newSize - pullDistanceFromEnd;
+
+        }
+
+        //int  = pushNum - pullNum;
+        //Object[] tempStack = java.util.Arrays.copyOf(queue, newSize);
+        Object[] tempQueue = new Object[newSize];
+
+        if (wrap) {
+            //for ( int i = 0 ; i < queue.length ;  i++ ) {
+
+            for (int i = 0; i < pushNum; i++) {
+                tempQueue[i] = queue[i];
+            }
+
+            // Skip the Blank Spaces
+            //for (int i = changeInSize + pullNum; i < tempQueue.length; i++) {
+            for (int i = pullNum; i < tempQueue.length; i++) {
+                tempQueue[i - changeInSize] = queue[i];
+            }
+        } else {
+            for (int i = 0; i < tempQueue.length; i++) {
+                tempQueue[i] = queue[i];
+            }
+        }
+
+        queue = tempQueue;
+        pushNum = newPush;
+        pullNum = newPull;
+
+        //Object[] tempStack = java.util.Arrays.copyOf(queue, newSize);
+        queue = tempQueue;
     }
 
-    private void considerGrowingStack() {
+    private void considerGrowingQueue() {
 
-        if (stack.length <= pointerNum + 1) {
+        int freeSpace = queue.length - size();
+        if (freeSpace <= 0) {
             growArray();
         }
     }
 
-    private void considerShrinkingStack() {
+    private void considerShrinkingQueue() {
 
-        int oneShrink = Math.round(stack.length * (1 - percentToChangeBy));
+        int freeSpaceNow = queue.length - size();
+
+        int oneShrink = Math.round(queue.length * (1 - percentToChangeBy));
+
+        int freeSpaceAfterOneShrink = oneShrink - size();
+
         int twoShrinks = Math.round(oneShrink * (1 - percentToChangeBy));
-        int halfWayBetween = Math.round((oneShrink + twoShrinks) / 2);
-
-        //System.out.println("Current Size: " + stack.length + "\t One Shrink: " + oneShrink + "\t Two Shrinks: " + twoShrinks + "\t Half: " + halfWayBetween);
-        if (halfWayBetween > pointerNum && oneShrink > minimumSize) {
+        
+        int freeSpaceAfterTwoShrinks = twoShrinks - size();
+        
+        //int halfWayBetween = Math.round((oneShrink + twoShrinks) / 2);
+        int halfWayBetweenFreeSpace = Math.round((freeSpaceAfterOneShrink + freeSpaceAfterTwoShrinks) / 2);
+        
+        int freeSpaceAfterOneAndAHalfShrinks = freeSpaceAfterOneShrink + ( freeSpaceAfterTwoShrinks / 2 );
+        
+        // When freespace after 1 1/2 shrinks is 0.
+        
+        if ( freeSpaceAfterOneAndAHalfShrinks < 0 && oneShrink > minimumSize ) {
             shrinkArray();
         }
     }
 
-//    @Override
-//    public void push(T element) {
-//        if (element != null) {
-//
-//            stack[pointerNum] = element;
-//            pointerNum++;
-//
-//            considerGrowingStack();
-//        }
-//    }
-//
-//    @Override
-//    public T pop() {
-//
-//        if (pointerNum >= 0) {
-//            pointerNum--;
-//
-//            if (pointerNum < 0){
-//                
-//                pointerNum = 0;
-//                return null;
-//            }
-//            
-//            considerShrinkingStack();
-//            return (T) stack[pointerNum];
-//        } else {
-//            return null;
-//        }
-//    }
-    
     @Override
     public boolean isEmpty() {
-//        if (pointerNum > 0) {
-//            return false;
-//        } else {
-//            return true;
-//        }
+        return pushNum == pullNum;
     }
 
     @Override
     public int size() {
-        //return (pointerNum);
+
+        int size;
+        boolean wrap;
+        
+        if (pushNum > pullNum) {
+            wrap = false;
+
+            // The occupied part does not wrap around.
+            // The new spaces will go on the end.
+            //newPull = pullNum;
+           // newPush = pushNum;
+           
+            size = pushNum - pullNum;
+           
+        } else {
+            wrap = true;
+
+            // The occupied part does wrap around.
+            // The new spaces will go in the middle.
+            //newPush = pushNum;
+
+            //changeInSize = newSize - queue.length;
+            int pullDistanceFromEnd = queue.length - pullNum;
+
+            size = pullDistanceFromEnd + pushNum;
+            
+            //newPull = newSize - pullDistanceFromEnd;
+
+        }
+        
+        return size;
     }
 
     @Override
     public void enqueue(T element) {
         if (element != null) {
 
-            stack[pointerNum] = element;
-            pointerNum++;
+            queue[pushNum] = element;
+            incrementPushNum();
 
-            considerGrowingStack();
+            considerGrowingQueue();
         }
 
+    }
+
+    private void incrementPushNum() {
+        pushNum++;
+
+        if (pushNum >= queue.length) {
+            pushNum = 0;
+        }
     }
 
     @Override
     public T dequeue() {
 
-        if (pointerNum >= 0) {
-            pointerNum--;
+        if (pullNum >= 0 && pullNum < queue.length) {
+            Object returnObject = queue[pullNum];
+            queue[pullNum] = null;
 
-            if (pointerNum < 0) {
-
-                pointerNum = 0;
-                return null;
-            }
-
-            considerShrinkingStack();
-            return (T) stack[pointerNum];
+            incrementPullNumber();
+            considerShrinkingQueue();
+            return (T) returnObject;
         } else {
             return null;
         }
         //return null;
     }
 
+    private void incrementPullNumber() {
+        pullNum++;
+
+        if (pullNum >= queue.length) {
+            pullNum = 0;
+        }
+    }
 
 }
