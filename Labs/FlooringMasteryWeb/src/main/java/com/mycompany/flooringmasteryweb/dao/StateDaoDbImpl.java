509c566f7a98d884c303138aebf3f2ba956e0a7c
Adam Tegtmeier <ategtmei@gmail.com>
2016-06-17 11:47:22 -0400

    ATSW-103, Flooring Master with SQL. Working on the Minor DAOs first.

--- Labs/FlooringMasteryWeb/src/main/java/com/mycompany/flooringmasteryweb/dao/StateDaoDbImpl.java
+++ Labs/FlooringMasteryWeb/src/main/java/com/mycompany/flooringmasteryweb/dao/StateDaoDbImpl.java
@@ -11,11 +11,18 @@ import com.mycompany.flooringmasteryweb.utilities.StateFileIO;
 import com.mycompany.flooringmasteryweb.utilities.StateFileIOImplementation;
 import com.mycompany.flooringmasteryweb.utilities.StateUtilities;
 import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.inject.Inject;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -23,21 +30,26 @@ import java.util.stream.Collectors;
  */
 public class StateDaoDbImpl implements StateDao {
 
-    private java.util.Map<String, State> statesMap;
-    private File stateDataFile = new File("StatesData.txt");
-    private StateFileIO fileIo;
+    //private NoteDao noteDao;
+    private JdbcTemplate jdbcTemplate;
 
-    public StateDaoDbImpl(ConfigDao configDao) {
+    private static final String SQL_INSERT_STATE = "INSERT INTO states ( state_name, state_abbreviation, tax_rate ) VALUES ( ?, ?, ? )";
+    //private static final String SQL_UPDATE_STATE = "UPDATE states SET state_name=?, state_abbreviation=?, tax_rate=? WHERE state_abbreviation=?";
+    private static final String SQL_UPDATE_STATE = "UPDATE states SET tax_rate=? WHERE state_abbreviation=?";
+    private static final String SQL_DELETE_STATE = "DELETE FROM states WHERE state_abbreviation =?";
+    private static final String SQL_GET_STATE = "SELECT * FROM states WHERE state_abbreviation =?";
+    private static final String SQL_GET_STATE_ID = "SELECT * FROM states WHERE id =?";
+    private static final String SQL_GET_STATE_LIST = "SELECT * FROM states";
+    
+//    @Inject
+//    public StateLibraryDbImplementation(NoteDao noteDao) {
+//        this(false, noteDao);
+//    }
+    @Inject
+    public StateDaoDbImpl(JdbcTemplate jdbcTemplate) {
 
-        this.fileIo = new StateFileIOImplementation(this);
-        stateDataFile = configDao.get().getTaxesFile();
-
-        statesMap = fileIo.decode(stateDataFile);
-
-        if (statesMap == null) {
-            statesMap = new java.util.HashMap();
-            System.out.println("The list was empty, making a new one.");
-        }
+        this.jdbcTemplate = jdbcTemplate;
+        //this.noteDao = noteDao;
     }
 
     @Override
@@ -73,95 +85,273 @@ public class StateDaoDbImpl implements StateDao {
 
             String postalCode = stateName.toUpperCase();
 
-            if (!statesMap.containsKey(postalCode)) {
+            //state_name, state_abbreviation, tax_rate
+            //first_name, last_name, street_number, street_name, city, state, zip
+            jdbcTemplate.update(SQL_INSERT_STATE,
+                    null,
+                    state.getStateName(),
+                    state.getStateTax());
 
-                state.setState(postalCode);
-                statesMap.put(postalCode, state);
+            Integer id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
 
-                fileIo.encode(stateDataFile, getList());
+            state.setId(id);
 
-                returnedState = state;
-            }
+//            if (!statesMap.containsKey(postalCode)) {
+//
+//                state.setState(postalCode);
+//                statesMap.put(postalCode, state);
+//
+//                fileIo.encode(stateDataFile, getList());
+//
+//                returnedState = state;
+//            }
         } else {
             // Look up how to throw exceptions and consider that here.
         }
         return returnedState;
     }
 
-    @Override
+//    @Override
+//    @Transactional(propagation = Propagation.REQUIRED)
+//    public State create(State state) {
+//
+//        if (state == null) {
+//            return null;
+//        }
+//
+//        //first_name, last_name, street_number, street_name, city, state, zip
+//        jdbcTemplate.update(SQL_INSERT_STATE,
+//                state.getTitle(),
+//                state.getReleaseDate(),
+//                state.getRating(),
+//                state.getDirectorsName(),
+//                state.getStudio());
+//
+//        Integer id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+//
+//        state.setId(id);
+//
+//        return state;
+//    }
+    //@Override
+    public State get(Integer id) {
+
+        if (id == null) {
+            return null;
+        }
+        try {
+            return jdbcTemplate.queryForObject(SQL_GET_STATE_ID, new StateMapper(), id);
+        } catch (org.springframework.dao.EmptyResultDataAccessException ex) {
+            return null;
+        }
+    }
+
     public State get(String name) {
 
         if (name == null) {
             return null;
         }
 
-        String input = null;
-        for (String stateTest : statesMap.keySet()) {
-            if (name.equalsIgnoreCase(stateTest)) {
-                input = stateTest;
-                break;
-            }
+        try {
+            return jdbcTemplate.queryForObject(SQL_GET_STATE, new StateMapper(), name);
+        } catch (org.springframework.dao.EmptyResultDataAccessException ex) {
+            return null;
         }
-        return statesMap.get(input);
     }
 
     @Override
     public void update(State state) {
 
-        if (state != null) {
-            State foundState = statesMap.get(state.getState());
-
-            if (foundState != null) {
-
-                if (foundState.getState().equals(state.getState())) {
-                    statesMap.remove(foundState.getState());
-                    statesMap.put(state.getState(), state);
-
-                    fileIo.encode(stateDataFile, getList());
-                } else {
-                    System.out.println("Throwing a State Not Found exception!!!!");
-                    // Look up exception throwing and consider putting one here, too!
-                }
-            } else {
-                create(state);
-                //System.out.println("Throwing a State is null exception!!!!");
-                // Look up exception throwing and consider putting one here, too!
-            }
+        //"UPDATE states SET tax_rate=? WHERE state_abbreviation=?";
+        if (state == null) {
+            return;
         }
+
+        if (state.getId() > 0) {
+
+            jdbcTemplate.update(SQL_UPDATE_STATE,
+                    state.getStateTax(),
+                    state.getStateName());
+        }
+
     }
 
     @Override
     public void delete(State state) {
-
-        if (statesMap.containsKey(state.getState())) {
-            statesMap.remove(state.getState());
-            fileIo.encode(stateDataFile, getList());
-        } else {
-            System.out.println("Throwing a State Not Found exception!!!!");
-            // Look up exception throwing and consider putting one here, too!
-
+        if (state == null) {
+            return;
         }
+
+        int id = state.getId();
+
+        jdbcTemplate.update(SQL_DELETE_STATE, id);
     }
+
+    private static final String SQL_GET_STATE_NAMES = "SELECT state_abbreviation FROM states";
 
     @Override
     public List<String> getList() {
-        return new ArrayList(statesMap.keySet());
+        return jdbcTemplate.query(SQL_GET_STATE_NAMES, new StateNameMapper());
+        //return getListOfStates();
     }
 
-    @Override
-    public int size() {
-        return statesMap.size();
+//    public void setNoteDao(NoteDao noteDao) {
+//        this.noteDao = noteDao;
+//    }
+    private final class StateMapper implements RowMapper<State> {
+
+        @Override
+        public State mapRow(ResultSet rs, int i) throws SQLException {
+
+            //state_name, state_abbreviation, tax_rate
+            State state = new State();
+
+            state.setId(rs.getInt("id"));
+            state.setStateName(rs.getString("state_abbreviation"));
+//            state.setStateName(rs.getString("state_name"));
+            //state.setReleaseDate(rs.getDate("state_abbreviation"));
+
+            try {
+                String taxString = rs.getString("tax_rate");
+
+                double tax = Double.parseDouble(taxString);
+                state.setStateTax(tax);
+
+            } catch (NullPointerException | NumberFormatException ex) {
+                state.setStateTax(0.0d);
+            }
+
+//            state.setDirectorsName(rs.getString("directors_name"));
+//            state.setStudio(rs.getString("studio"));
+//
+//            List<Note> notes = noteDao.getNotesForState(state);
+            //state.setNotes(notes);
+            return state;
+        }
+
+    }
+    
+    private final class StateNameMapper implements RowMapper<String> {
+
+        @Override
+        public String mapRow(ResultSet rs, int i) throws SQLException {
+
+            //state_name, state_abbreviation, tax_rate
+            
+            String stateName = rs.getString("state_name");
+            
+//            state.setId(rs.getInt("id"));
+//            state.setStateName(rs.getString("state_name"));
+//            //state.setReleaseDate(rs.getDate("state_abbreviation"));
+//
+//            try {
+//                String taxString = rs.getString("tax_rate");
+//
+//                double tax = Double.parseDouble(taxString);
+//                state.setStateTax(tax);
+//
+//            } catch (NullPointerException | NumberFormatException ex) {
+//                state.setStateTax(0.0d);
+//            }
+
+//            state.setDirectorsName(rs.getString("directors_name"));
+//            state.setStudio(rs.getString("studio"));
+//
+//            List<Note> notes = noteDao.getNotesForState(state);
+            //state.setNotes(notes);
+            return stateName;
+        }
+
     }
 
     @Override
     public List<State> getListOfStates() {
 
-        List<State> states = getList().stream()
-                .map(name -> get(name))
-                .collect(Collectors.toList());
-
-        return states;
+        return jdbcTemplate.query(SQL_GET_STATE_LIST, new StateMapper());
     }
+
+    private static final String SQL_COUNT_STATES = "SELECT COUNT(*) FROM states";
+
+    @Override
+    public int size() {
+        return jdbcTemplate.queryForObject(SQL_COUNT_STATES, Integer.class);
+    }
+//
+//
+//    @Override
+//    public State get(String name) {
+//
+//        if (name == null) {
+//            return null;
+//        }
+//
+//        String input = null;
+//        for (String stateTest : statesMap.keySet()) {
+//            if (name.equalsIgnoreCase(stateTest)) {
+//                input = stateTest;
+//                break;
+//            }
+//        }
+//        return statesMap.get(input);
+//    }
+//
+//    @Override
+//    public void update(State state) {
+//
+//        if (state != null) {
+//            State foundState = statesMap.get(state.getState());
+//
+//            if (foundState != null) {
+//
+//                if (foundState.getState().equals(state.getState())) {
+//                    statesMap.remove(foundState.getState());
+//                    statesMap.put(state.getState(), state);
+//
+//                    fileIo.encode(stateDataFile, getList());
+//                } else {
+//                    System.out.println("Throwing a State Not Found exception!!!!");
+//                    // Look up exception throwing and consider putting one here, too!
+//                }
+//            } else {
+//                create(state);
+//                //System.out.println("Throwing a State is null exception!!!!");
+//                // Look up exception throwing and consider putting one here, too!
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void delete(State state) {
+//
+//        if (statesMap.containsKey(state.getState())) {
+//            statesMap.remove(state.getState());
+//            fileIo.encode(stateDataFile, getList());
+//        } else {
+//            System.out.println("Throwing a State Not Found exception!!!!");
+//            // Look up exception throwing and consider putting one here, too!
+//
+//        }
+//    }
+//
+//    @Override
+//    public List<String> getList() {
+//        return new ArrayList(statesMap.keySet());
+//    }
+//
+//    @Override
+//    public int size() {
+//        return statesMap.size();
+//    }
+//
+//    @Override
+//    public List<State> getListOfStates() {
+//
+//        List<State> states = getList().stream()
+//                .map(name -> get(name))
+//                .collect(Collectors.toList());
+//
+//        return states;
+//    }
 
     @Override
     public List<State> sortByStateName(List<State> states) {
