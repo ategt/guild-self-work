ed59bae45b465a43e4b78a69e9b35aac682c48f6
Adam Tegtmeier <ategtmei@gmail.com>
2016-06-24 18:16:33 -0400

    WD-52 Redoing it with spring.io stuff.

---
+++ FileWebIOTest/TestFileWebIO/src/main/java/com/mycompany/testfilewebio/Application.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testfilewebio;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+}
--- FileWebIOTest/TestFileWebIO/src/main/java/com/mycompany/testfilewebio/FileUploadController.java
+++ FileWebIOTest/TestFileWebIO/src/main/java/com/mycompany/testfilewebio/FileUploadController.java
@@ -1,59 +1,78 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
  */
 package com.mycompany.testfilewebio;
 
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindException;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.mvc.SimpleFormController;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FileUploadController {
 
-    FileUpload fileUpload;
-    
-    @Inject
-    public FileUploadController(FileUpload fileUpload) {
-        
-        this.fileUpload = fileUpload;
-        
-//        setCommandClass(FileUpload.class);
-//        setCommandName("fileUploadForm");
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String provideUploadInfo(Model model) {
+        File rootFolder = new File(Application.ROOT);
+        List<String> fileNames = Arrays.stream(rootFolder.listFiles())
+                .map(f -> f.getName())
+                .collect(Collectors.toList());
+
+        model.addAttribute("files",
+                Arrays.stream(rootFolder.listFiles())
+                .sorted(Comparator.comparingLong(f -> -1 * f.lastModified()))
+                .map(f -> f.getName())
+                .collect(Collectors.toList())
+        );
+
+        return "uploadForm";
     }
 
-    //@Override
-    protected ModelAndView onSubmit(HttpServletRequest request,
-            HttpServletResponse response, Object command, BindException errors)
-            throws Exception {
-
-        FileUpload file = (FileUpload) command;
-
-        MultipartFile multipartFile = file.getFile();
-
-        String fileName = "";
-
-        if (multipartFile != null) {
-            fileName = multipartFile.getOriginalFilename();
-            //do whatever you want
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public String handleFileUpload(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file,
+            RedirectAttributes redirectAttributes) {
+        if (name.contains("/")) {
+            redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
+            return "redirect:/";
+        }
+        if (name.contains("/")) {
+            redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
+            return "redirect:/";
         }
 
-        return new ModelAndView("FileUploadSuccess", "fileName", fileName);
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(new File(Application.ROOT + "/" + name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+                stream.close();
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded " + name + "!");
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message",
+                        "You failed to upload " + name + " => " + e.getMessage());
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("message",
+                    "You failed to upload " + name + " because the file was empty");
+        }
+
+        return "redirect:/";
     }
 
-//    @Override
-//    protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder)
-//            throws ServletException {
-//
-//        // Convert multipart object to byte[]
-//        binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
-//
-//    
 }
